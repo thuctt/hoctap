27cb64dc5312f11c2587c2e56263c0b2540f4ea9
thuctt <thuctt@users.noreply.github.com>
2015-03-25 15:54:01 +0700

    update bai tap Calculator-thuctt.py

--- calcu-python.py
+++ calcu-python.py
@@ -1,33 +1,59 @@
-# Program make a simple calculator that can add, subtract, multiply and divide using functions
-# define functions
-def add(x,y):
-#"""This function adds two numbers"""
-    return x+y
-def subtract(x,y):
-#"""This function subtracts two numbers"""
-    return x-y
-def multiply(x,y):
-#"""This function multiplies two numbers"""
-    return x*y
-def divide(x,y):
-#"""This function divides two numbers"""
-   return x/y
-# take input from the user print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-#########
-choice = input("Enter choice(1/2/3/4):")
-num1 = float(raw_input("Enter first number: "))
-num2 = float(raw_input("Enter second number: "))
-if choice == '1':
-         print(num1,"+",num2,"=", add(num1,num2))
-elif choice == '2':
-         print(num1,"-",num2,"=", subtract(num1,num2))
-elif choice == '3':
-         print(num1,"*",num2,"=", multiply(num1,num2))
-elif choice == '4':
-         print(num1,"/",num2,"=", divide(num1,num2))
-else:
-         print("Invalid input")
+###########
+class bcolors:
+  OKGREEN = '\033[92m'
+  HEADER = '\033[95m'
+  UNDERLINE = '\033[4m'
+running = True
+while running:
+    print '\033[1;31m-------------------- \033[1;m'
+    print '\033[1;31mNhap phep tinh Em oi \033[1;m'
+    print "1.Cong"
+    print "2.Tru"
+    print "3.Nhan"
+    print "4.Chia"
+    print "5.Fibonacci"
+    print "6.Thoat chuong trinh"
+    choice = raw_input('Chon (1-6): ')
+# Thuc hien phep tinh
+    if choice == "1":
+             num1 = float(raw_input('Moi Ban nhap so thu 1: '))
+             num2 = float(raw_input('Moi Ban nhap so thu 2: '))
+             print '\033[1;31m-------------------- \033[1;m'
+             print '\033[1;32mTong= \033[1;m',num1+num2
+    elif choice == "2":
+             num1 = float(raw_input('Moi Ban nhap so thu 1: '))
+             num2 = float(raw_input('Moi Ban nhap so thu 2: '))
+             print '\033[1;31m-------------------- \033[1;m'
+             print '\033[1;32mHieu= \033[1;m', num1-num2
+    elif choice == "3":
+             num1 = float(raw_input('Moi Ban nhap so thu 1: '))
+             num2 = float(raw_input('Moi Ban nhap so thu 2: '))
+             print '\033[1;31m-------------------- \033[1;m'
+             print '\033[1;32mTich= \033[1;m', num1*num2
+    elif choice == "4":
+             num1 = float(raw_input('Moi Ban nhap so thu 1: '))
+             num2 = float(raw_input('Moi Ban nhap so thu 2: '))
+             while num2 == 0:
+                     num2 = float(raw_input('Nhap lai:'))
+             print '\033[1;31m-------------------- \033[1;m'
+             print '\033[1;32mThuong= \033[1;m', num1/num2
+#Tinh Fibonacci			 
+    elif choice == "5":
+             x = int(raw_input('Tinh F(n), Moi ban nhap x: '))
+             num1=0
+             num2=1
+             i=1
+             f=0
+             if x==0 or x==1:
+                     y=x
+             else:
+               while i<x:
+                     y=num1+num2
+                     num1=num2
+                     num2=y
+                     i=i+1
+             print '\033[1;31m-------------------- \033[1;m'
+             print 'F('+str(x)+')= ', y
+# Thoat chuong trinh			 
+    elif choice == "6":
+         running = False
